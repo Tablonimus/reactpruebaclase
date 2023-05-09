33d7fcb040fe5ae6d1ec49d364f814e1e0c538d0
tablonimus <lautamartin1995@gmail.com>
2023-05-09 19:59:32 -0300

    borro todo

--- src/Components/Buttons/RoundedButton.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../../Styles/roundedButton.css";
-
-export default function RoundedButton({ contenido, contenido2 }) { //  {}   el propp
-  return <button className="roundedButton">{contenido}</button>;
-}
