0a3f36d0630f63ea64f5881fb0b607606dcade10
tablonimus <lautamartin1995@gmail.com>
2023-05-23 22:50:12 -0300

    el finalde verdad

--- app22web/src/Controllers/api.js
+++ app22web/src/Controllers/api.js
@@ -31,30 +31,30 @@ export async function pedirPokemones(numero) {
   return arrayDePokemones;
 } //ESTA FUNCION ME RETORNA UNA ARRAY DE POKEMONES
 
-export async function buscarPorNombre(pokemonName) {
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+// export async function buscarPorNombre(pokemonName) {
+//   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-  const caracteristicas = await data.json();
+//   const caracteristicas = await data.json();
 
-  const nombre = caracteristicas.name; //"string"
-  const imagen = caracteristicas.sprites.other.dream_world.front_default; //"string"
-  const tipo = caracteristicas.types.map((objeto) => objeto.type.name); //"string"
+//   const nombre = caracteristicas.name; //"string"
+//   const imagen = caracteristicas.sprites.other.dream_world.front_default; //"string"
+//   const tipo = caracteristicas.types.map((objeto) => objeto.type.name); //"string"
 
-  const pokemonHTML = `
-        <div class="pokemonCard">
-        <img src="  ${imagen} " />
-            <div>
-               <span id="nombrePokemon"> ${nombre} </span>
-            </div>
-             <div id="tipos-container"> 
-                <span>Tipo</span>
-                <span id="tipoPokemon"> ${tipo} </span>
-             </div>                
-        </div>
-        `;
+//   const pokemonHTML = `
+//         <div class="pokemonCard">
+//         <img src="  ${imagen} " />
+//             <div>
+//                <span id="nombrePokemon"> ${nombre} </span>
+//             </div>
+//              <div id="tipos-container"> 
+//                 <span>Tipo</span>
+//                 <span id="tipoPokemon"> ${tipo} </span>
+//              </div>                
+//         </div>
+//         `;
 
-  root.innerHTML += pokemonHTML;
-}
+//   root.innerHTML += pokemonHTML;
+// }
 
 export async function buscarDetalle(id) {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
